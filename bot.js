cffdf6a92a4df20a2a0a24c1729d7f45278904ff
Youngmin Park <14ypark@gmail.com>
2016-10-12 11:28:20 -0500

    removed auth json

--- bot.js
+++ bot.js
@@ -1,4 +1,24 @@
 var Discord = require("discord.js")
-var myBot = new Discord.Client();
 var fs = require('fs');
 var auth = require("./auth.json")
+var token = auth.bot_token;
+
+var myBot = new Discord.Client({
+  autorun: true
+});
+
+var prefix = '!';
+
+
+myBot.on('message', message => {
+  if (message.content === prefix + 'y') {
+    message.channel.sendMessage('yeon joon sucks at hanzo');
+  }
+
+});
+
+
+myBot.login(token);
+myBot.on('ready', ()=>{
+  console.log("it is running");
+})
